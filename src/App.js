6868a3736c0c241ea7c310092b145d8b1b0632a5
Agit <agtt@users.noreply.github.com>
2019-07-19 00:08:43 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log("Triggered");
-  });
+    const testfunc = a => {
+      console.log("Use Effect Test");
+    };
+    testfunc();
+  }, []);
 
   return (
     <div className="App">
