cc5db72bdc61be09e19d9bc9fc0fad2092685b1c
Agit <agtt@users.noreply.github.com>
2019-07-19 00:43:48 +0300

    f

--- src/Layouts/TodoList.js
+++ src/Layouts/TodoList.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
+import { Image, List } from "semantic-ui-react";
 
 const TodoList = () => {
-  const [todos, addTodo] = useState("");
+  const [todos, addTodo] = useState([]);
 
-  return <h1>Todo List</h1>;
+  return (
+    <List divided verticalAlign="middle">
+      {todos.map(todo => {
+        return (
+          <List.Item>
+            <Image
+              avatar
+              src="https://react.semantic-ui.com/images/avatar/small/daniel.jpg"
+            />
+            <List.Content>
+              <List.Header as="a">{todo}</List.Header>
+            </List.Content>
+          </List.Item>
+        );
+      })}
+    </List>
+  );
 };
 
 export default TodoList;
