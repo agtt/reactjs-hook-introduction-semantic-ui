32990133ab4547adae16c22b1652b5efabd1ed81
Agit <agtt@users.noreply.github.com>
2019-07-20 02:56:14 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import Reducers from "./Redux/Reducers";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Layouts/Home";
@@ -13,27 +14,30 @@ import TodoList from "./Layouts/TodoList";
 import { Container } from "semantic-ui-react";
 import Login from "./Layouts/Login";
 
-const store = createStore(Reducers);
+const store = createStore(Reducers, applyMiddleware(thunk));
 
 const Routers = () => {
   return (
-    <Provider store={store}>
-      <Container style={{ margin: 20 }}>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/users" component={Users} />
-            <Route exact path="/todos" component={TodoList} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </Router>
-        <Footer />
-      </Container>
-    </Provider>
+    <Container style={{ margin: 20 }}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/todos" component={TodoList} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+      <Footer />
+    </Container>
   );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Routers />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <Routers />
+  </Provider>,
+  rootElement
+);
